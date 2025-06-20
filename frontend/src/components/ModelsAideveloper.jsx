ca91630f37043301f531737f8b157ab6c32836e8
amantechDevise <amantechdevise@gmail.com>
2025-06-20 14:46:25 +0530

    loction page set

--- frontend/src/components/ModelsAideveloper.jsx
+++ frontend/src/components/ModelsAideveloper.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
+  import '../styles/components/AnalystRecognition.css'
 
 const ModelsAideveloper = () => {
   return (
     <div className="w-full max-w-[1920px] h-auto m-auto bg-gradient-to-r from-white to-[#BBD2DC] flex flex-col items-center justify-center px-10 mt-10">
       <div className="text-center mb-10">
-        <h1 className="md:text-[48px]  text-[25px] font-Montserrat font-extrabold text-black leading-snug">
+        <h1 className="md:text-[48px]  text-[25px] font-Montserrat font-extrabold text-black leading-snug pTAG">
         Models our AI Developers are Proficient with
         </h1>
-        <p className="text-[#1f1f1f] text-[15px] md:text-[20px] font-medium mt-6 max-w-[800px] mx-auto leading-relaxed">
+        <p className="text-[#1f1f1f] text-[15px] md:text-[20px] font-medium mt-6 max-w-[800px] mx-auto leading-relaxed ">
         As a leading Blockchain development company, TechDevise only hires Blockchain developers with a proven track record. That’s why we can handle any task or project, even the most complicated ones.
         </p>
       </div>
